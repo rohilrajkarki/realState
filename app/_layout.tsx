d9017447c73e741309a4c08c1cf96034156dca6c
rohilrajkarki <rohilrajkarki@gmail.com>
2025-02-24 11:26:09 +0800

    added fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,16 @@
 import { Stack } from "expo-router";
 import "./global.css";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+  });
+
   return <Stack />;
 }
