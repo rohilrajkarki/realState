463e9dda5cd681654d78209c0d94430590918f5f
rohilrajkarki <rohilrajkarki@gmail.com>
2025-03-26 18:17:52 +0800

    Updated tab bar

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,9 +1,38 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { logout } from "@/app/lib/appwrite";
 import { Tabs } from "expo-router";
+import icons from "@/constants/icons";
 
 const TabsLayout = () => {
+  const TabIcon = ({
+    icon,
+    title,
+    focused,
+  }: {
+    icon: any;
+    title: string;
+    focused: boolean;
+  }) => (
+    <View className="flex-1 mt-3 flex flex-col items-center">
+      <Image
+        source={icon}
+        tintColor={focused ? "#0061ff" : "#666876"}
+        resizeMode="contain"
+        className="size-6"
+      />
+      <Text
+        className={` ${
+          focused
+            ? "text-primary-300 font-rubik-medium"
+            : "text-black-200 font-rubik"
+        } text-xs w-full text-center mt-1`}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+
   return (
     <Tabs
       screenOptions={{
@@ -18,14 +47,30 @@ const TabsLayout = () => {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="index" //name of file i.e index.tsx
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: () => <TabIcon icon={icons.home} title="Home" focused />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
         options={{
           title: "Home",
           headerShown: false,
           tabBarIcon: () => (
-            <View>
-              <Text>Home</Text>
-            </View>
+            <TabIcon icon={icons.home} title="Explore" focused />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "profile",
+          headerShown: false,
+          tabBarIcon: () => (
+            <TabIcon icon={icons.home} title="Profile" focused />
           ),
         }}
       />
