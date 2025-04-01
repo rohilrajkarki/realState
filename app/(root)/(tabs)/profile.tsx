7bc2fc4fa2f5a42a73f088e35fa0adc9eeb1c737
rohilrajkarki <rohilrajkarki@gmail.com>
2025-04-01 21:52:17 +0800

    Updated profile page

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,13 +1,71 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  ImageSourcePropType,
+} from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+
+interface SettingsItemProps {
+  icon: ImageSourcePropType;
+  title: string;
+  onPress: () => void;
+  textStyle?: any;
+  showArrow?: boolean;
+}
+
+const SettingsItem = ({
+  icon,
+  title,
+  onPress,
+  textStyle,
+  showArrow = true,
+}: SettingsItemProps) => (
+  <TouchableOpacity>
+    <View>
+      <Image source={icon} />
+      <Text>{title}</Text>
+    </View>
+  </TouchableOpacity>
+);
 
 const Profile = () => {
   const handleLogout = async () => {};
 
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 px-7"
+      >
+        <View className="flex flex-row items-center justify-between mt-5">
+          <Text className="text-xl font-rubik-bold">Profile</Text>
+          <Image source={icons.bell} className="size-5" />
+        </View>
+        <View className="flex-row justify-center flex mt-5">
+          <View className="flex flex-col items-center relative mt-5">
+            <Image
+              source={images.avatar}
+              className="size-44 relative rounded-full"
+            />
+            <TouchableOpacity className="absolute bottom-11 right-2">
+              <Image source={icons.edit} className="size-9 " />
+            </TouchableOpacity>
+            <Text className="text-2xl font-rubik-bold mt-2">
+              Rohil Raj Karki
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+      <View className="flex flex-col mt-10">
+        <SettingsItem icon={icons.calendar} title="My Booking" />
+      </View>
+    </SafeAreaView>
   );
 };
 
