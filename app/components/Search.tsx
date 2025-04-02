3f7e69001c3c9b93c4e56b2244911559d09b798f
rohilrajkarki <rohilrajkarki@gmail.com>
2025-04-02 19:52:46 +0800

    updated search component

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,7 +1,16 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { useLocalSearchParams, usePathname } from "expo-router";
 
 const Search = () => {
+  const path = usePathname();
+  const params = useLocalSearchParams<{ query: string }>();
+  const [search, setSearch] = useState(params.query);
+
+  const handleSearch = (text: string) => {
+    setSearch(text);
+  };
+
   return (
     <View>
       <Text>Search</Text>
