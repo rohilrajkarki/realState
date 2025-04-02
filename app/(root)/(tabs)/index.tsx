7add328136cf31f1a77a9bfceac5ccad15b583cb
rohilrajkarki <rohilrajkarki@gmail.com>
2025-04-02 19:49:53 +0800

    Created custom search component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,27 +1,32 @@
+import Search from "@/app/components/Search";
 import { logout } from "@/app/lib/appwrite";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-3xl my-10">Welcome To RealState</Text>
-      {/* <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Properties</Link> */}
-      <TouchableOpacity
-        onPress={logout}
-        className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
-      >
-        <Text>Logout</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                Rohil
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+      </View>
+      <Search />
+    </SafeAreaView>
   );
 }
